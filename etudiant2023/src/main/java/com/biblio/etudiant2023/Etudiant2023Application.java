00c8cbbf7d6e3f9aeb83717dca0444fcef09a5a7
lefortnuno <trofelnuno@gmail.com>
2026-01-26 23:23:39 +0100

    feat: Etudiant-2

--- etudiant2023/src/main/java/com/biblio/etudiant2023/Etudiant2023Application.java
+++ etudiant2023/src/main/java/com/biblio/etudiant2023/Etudiant2023Application.java
@@ -1,52 +1,58 @@
 package com.biblio.etudiant2023;
 
-import com.biblio.etudiant2023.config.GlobalConfig; 
 import com.biblio.etudiant2023.entities.Etudiant;
 import com.biblio.etudiant2023.repositories.EtudiantRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Etudiant2023Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Etudiant2023Application.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context =
+                SpringApplication.run(Etudiant2023Application.class, args);
+
+        // ferme l'app proprement après le runner
+        SpringApplication.exit(context, () -> 0);
+    }
 
     @Bean
     CommandLineRunner run(EtudiantRepository etudiantRepository) {
         return args -> {
-            
-            System.out.println("Hello, Trofel! \n");
-            Etudiant std1 = Etudiant.builder()
-                    .firstName("John")
-                    .lastName("Doe")
-                    .email("Johndoe@gmail.com")
-                    .age(25)
-                    .build();
-            etudiantRepository.save(std1);
 
-            Etudiant std2 = Etudiant.builder()
-                    .firstName("Saad")
-                    .lastName("Mohamed")
-                    .email("Saadmohammed@gmail.com")
-                    .age(18)
-                    .build();
-            etudiantRepository.save(std2);
+            System.out.println("\n\n\n\n\n\n\nHello, Trofel!\n");
 
-            Etudiant std3 = Etudiant.builder()
-                    .firstName("Widad")
-                    .lastName("Mohammed")
-                    .email("widadmohamed@gmail.com")
-                    .age(35)
-                    .build();
-            etudiantRepository.save(std3);
-            
-            System.out.println("Etudiant Crée! \n");
+            etudiantRepository.save(
+                    Etudiant.builder()
+                            .firstName("John")
+                            .lastName("Doe")
+                            .email("Johndoe@gmail.com")
+                            .age(25)
+                            .build()
+            );
+
+            etudiantRepository.save(
+                    Etudiant.builder()
+                            .firstName("Saad")
+                            .lastName("Mohamed")
+                            .email("Saadmohammed@gmail.com")
+                            .age(18)
+                            .build()
+            );
+
+            etudiantRepository.save(
+                    Etudiant.builder()
+                            .firstName("Widad")
+                            .lastName("Mohammed")
+                            .email("widadmohamed@gmail.com")
+                            .age(35)
+                            .build()
+            );
+
+            System.out.println("Étudiants créés. Fermeture de l'application... \n\n\n\n\n\n\n");
         };
     }
-
 }
